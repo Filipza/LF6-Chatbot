750d93ee8a2f576dbb7dac1fdb77db924662a1bb
Faby <fmlabus@gmail.com>
2023-06-05 17:02:31 +0200

    create first conversation options

--- js/chat.js
+++ js/chat.js
@@ -1,27 +1,102 @@
-let submitForm = document.querySelector('form')
-let textInput = document.querySelector('textarea')
-submitForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log(textInput.value)
-  createChatbox(true, textInput.value)
-  createChatbox(false, "hello world")
-  e.target.reset()
-})
+const submitForm = document.querySelector("form");
+const textInput = document.querySelector("textarea");
+const chatContentArea = document.querySelector(".chat-content-area");
+const orderKeywords = [
+  "bestellungen",
+  "retouren",
+  "retoure",
+  "bestellung",
+  "zurückschicken",
+];
+const helpKeywords = ["hilfe", "kontakt", "anrufen"];
+const menuKeywords = ["home", "menü", "anfang"];
 
-function createChatbox(isUser, message) {
-  const chatBoxType = isUser ? 'user' : 'gpt'
-  let answerTemplate = `<div class="${chatBoxType}-chat-box">
-  <div class="sub-chat-box">
-  <div class="chat-icon">
-    <img class="chatgpt-icon" src="images/${chatBoxType}-icon.png" />
-  </div>
-  <div class="chat-txt">${message}</div>
-</div>
-</div>`
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createUserChatbox(textInput.value);
+  checkInput(textInput.value);
+  e.target.reset();
+});
 
-  document.querySelector('.chat-content-area').insertAdjacentHTML("beforeend", answerTemplate)
-  console.log(document.querySelector('.chat-content-area'));
+function checkInput(input) {
+  const keywords = [...orderKeywords, ...helpKeywords, ...menuKeywords];
+  for (const keyword of keywords) {
+    console.log(input.toLowerCase().includes(keyword));
+    if (input.toLowerCase().includes(keyword)) {
+      if (orderKeywords.includes(keyword)) {
+        createSupportChatbox(
+          "Dir gehts also um Bestellungen, ja? Wähle eine der folgenden Themen",
+          ["Bestellstatus überprüfen", "Retouren"]
+        );
+        return;
+      } else if (helpKeywords.includes(keyword)) {
+        createSupportChatbox("Wie können wir dir helfen?", [
+          "FAQ",
+          "Telefonservice",
+        ]);
+        return;
+      } else if (menuKeywords.includes(keyword)) {
+        createSupportChatbox("Wie kann ich dir behilflich sein?", [
+          "Bestellungen",
+          "Hilfe",
+        ]);
+        return "menu";
+      }
+    }
+  }
 
-  let lastChild = document.querySelector('.chat-content-area').lastChild
-  lastChild.scrollIntoView({block:"center"});
+  createSupportChatbox(
+    "Leider konnte deine Anfrange nicht bearbeitet werden! Bei welchem Thema kann ich dir weiterhelfen?",
+    ["Bestellungen/Retouren", "Allgemeine Hilfe/FAQ"]
+  );
+}
+
+function createUserChatbox(message) {
+  const template = `<div class="user-chat-box">
+    <div class="sub-chat-box">
+      <div class="chat-icon">
+        <img class="chatgpt-icon" src="images/user-icon.png" />
+      </div>
+      <div class="chat-txt">${message}</div>
+    </div>
+  </div>`;
+
+  chatContentArea.insertAdjacentHTML("beforeend", template);
+  chatContentArea.lastChild.scrollIntoView({ block: "center" });
+}
+
+function createSupportChatbox(message, pills) {
+  // hide pills
+  const supportChatBoxes = document.querySelectorAll(
+    ".gpt-chat-box .chat-options"
+  );
+  supportChatBoxes.forEach((box) => {
+    box.remove();
+  });
+
+  let pillsTemplate = "";
+  if (pills.length > 0) {
+    for (const pill of pills) {
+      pillsTemplate += `<button class="option-btn p-1 me-2 btn btn-light">
+        ${pill}
+      </button>`;
+    }
+  }
+  // create chatbox
+  const template = `<div class="gpt-chat-box">
+      <div class="sub-chat-box">
+        <div class="chat-icon">
+          <img class="chatgpt-icon" src="images/gpt-icon.png" />
+        </div>
+        <div class="chat-txt">
+          ${message}
+          <div class="chat-options d-flex mt-3">
+            ${pillsTemplate}
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  chatContentArea.insertAdjacentHTML("beforeend", template);
+  chatContentArea.lastChild.scrollIntoView({ block: "center" });
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import '../style.scss';
 import * as bootstrap from 'bootstrap';
 
-(async function () {
-  const res = await fetch('http://localhost:3000/orders');
-  const data = await res.json();
-  console.log(data);
-})();
+// (async function () {
+//   const res = await fetch('http://localhost:3000/orders');
+//   const data = await res.json();
+//   console.log(data);
+// })();
