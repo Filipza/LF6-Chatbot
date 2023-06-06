0de28681e70e94cee879eebf9ad748c401f69c92
Faby <fmlabus@gmail.com>
2023-06-06 13:25:02 +0200

    add constants.js

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+export const faqText = `Frage 1: Wie kann ich eine Bestellung aufgeben? <br>
+Antwort: Sie können eine Bestellung bei uns aufgeben, indem Sie entweder unsere Website besuchen und das entsprechende Bestellformular ausfüllen oder indem Sie uns telefonisch kontaktieren und Ihre Bestellung durchgeben.
+<br>
+<br>
+Frage 2: Wie lange dauert es, bis meine Bestellung bearbeitet wird?<br>
+Antwort: Wir bemühen uns, Bestellungen so schnell wie möglich zu bearbeiten. In der Regel erfolgt die Bearbeitung innerhalb von 1-2 Werktagen. Bei besonders komplexen Bestellungen oder bei Engpässen kann es jedoch etwas länger dauern.
+<br>
+<br>
+Frage 3: Kann ich den Status meiner Bestellung verfolgen?<br>
+Antwort: Ja, dies ist möglich mit der Hilfe von unserem Kundenservice Bot.
+<br>
+<br>
+Frage 4: Was passiert, wenn ein bestellter Artikel nicht verfügbar ist?<br>
+Antwort: Sollte ein bestellter Artikel nicht verfügbar sein, werden wir Sie umgehend kontaktieren und Ihnen alternative Optionen anbieten. Wir streben stets an, Ihre Bestellung vollständig auszuführen, und bemühen uns, Ihnen ähnliche Produkte mit vergleichbaren Spezifikationen anzubieten.
+<br>
+<br>
+Frage 5: Was sind die Zahlungsmöglichkeiten für meine Bestellung?<br>
+Antwort: Wir akzeptieren verschiedene Zahlungsmethoden, darunter Kreditkarten, Banküberweisungen und PayPal. Die genauen Zahlungsoptionen werden Ihnen während des Bestellvorgangs angezeigt. Bitte beachten Sie, dass Zahlungen vor dem Versand der Bestellung erforderlich sind.`;
